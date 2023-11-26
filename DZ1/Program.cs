bc0bd7eb5400c6a86e99b621ab77688db51f9b43
Rustam <autocentr@list.ru>
2023-11-26 20:26:27 +0500

    no

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿//Задача 64: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 5 -> "2, 4"
+//M = 4; N = 8 -> "4, 6, 8"
+//1
+
+   Console.Write("Введите значение M: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите значение N: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"Чётные числа от {m} до {n}:");
+    evenNumbers(m, n);
+
+void evenNumbers(int start, int end)
+{
+        if (start > end)
+    {
+        return;
+    }
+    if (start % 2 == 0)
+    {
+        Console.WriteLine(start);
+    }
+
+ 
+    evenNumbers(start + 1, end);
+}
--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+//1
+
+
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum = CalcSum(m, n);
+
+Console.WriteLine($"Сумма натуральных чисел от {m} до {n}: {sum}");
+
+
+int CalcSum(int start, int end)
+{
+    if (start > end)
+    {
+        return 0;
+    }
+    return start + CalcSum(start + 1, end);
+}
